55a4876f552b1466ed535ecf8246727e4a6852b9
xcornx <yilluminati1337@gmail.com>
2021-03-16 12:11:35 +0000

    Initial

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,17 @@
 import random
 
 def main():
-	dice_rolls = 2
+	dice_rolls = int(input('Berapa dadu? :')) 
 	dice_sum = 0
 	for i in range(0,dice_rolls):
 		roll = random.randint(1,6)
 		dice_sum += roll
-		print(f'Rolled : {roll}')
+		if roll == 1:
+			print(f'Rolled: {roll}! Lose')
+		elif roll == 6:
+			print(f'Rolled: {roll}! Jackpot')
+		else:
+			print(f'Rolled : {roll}')
 	print(f'Total: {dice_sum}')
 
 if __name__== "__main__":
